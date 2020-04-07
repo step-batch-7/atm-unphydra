2e09f17ea7aaf91c6e7c413541cf34657f78a260
Srijayanth Sridhar <131062+craftybones@users.noreply.github.com>
2020-04-07 12:34:59 +0530

    Modifying main to send unsigned short int

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-  int amounts[] = {1, 10, 39, 589, 2590, 31999};
+  unsigned short int amounts[] = {1, 10, 39, 589, 2590, 31999};
   for (int i = 0; i < 6; i++)
   {
     unsigned int notes = get_money(amounts[i]);
